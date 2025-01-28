fdbb0e32a04918525fabedeb18f018836c56b10c
Sharvani Yadav <s1yadav@torontomu.ca>
2025-01-28 18:57:07 -0500

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "syadavwork25@gmail.com";  // Your email address
+    $subject = "New Contact Form Submission from $name";
+    $headers = "From: $email\r\nReply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Phone: $phone\n";
+    $body .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
